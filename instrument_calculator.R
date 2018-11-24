2538d0f9050f2bd69881d8599e34acd129a94793
clairebogdan <bogdanclaire@gmail.com>
2018-11-24 16:30:45 -0500

    third commit

--- instrument_calculator.R
+++ instrument_calculator.R
@@ -14,10 +14,27 @@ p_clarinet = .26
 p_bclar = .02
 p_alto = .06
 p_tenor = .01
-p_bari = 0.1
+p_bari = 0.01
 
 #Step 2: Define the total beginners
-n_beginners = 100
+n_beginners = 150
 
 #Step 3: Display the number of students on each instrument
+print(paste("For a beginning class of",n_beginners, "you should have..."))
+print(paste(p_flute*n_beginners,"Flutes"))
+print(paste(p_oboe*n_beginners,"Oboes"))
+print(paste(p_bassoon*n_beginners,"Bassoons"))
+print(paste(p_clarinet*n_beginners,"Clarinets"))
+print(paste(p_bclar*n_beginners,"Bass Clarinets"))
+print(paste(p_alto*n_beginners,"Alto Saxophones"))
+print(paste(p_tenor*n_beginners,"Tenor Saxophones"))
+print(paste(p_bari*n_beginners,"Bari Saxophones"))
+print(paste(p_trumpet*n_beginners,"Trumpets"))
+print(paste(p_horn*n_beginners,"French Horns"))
+print(paste(p_trombone*n_beginners,"Trombones"))
+print(paste(p_euphonium*n_beginners,"Euphoniums"))
+print(paste(p_tuba*n_beginners,"Tubas"))
+print(paste(p_percussion*n_beginners,"Percussionists"))
+
+
 
